9c993d6651f517aebe9e049e834a01b82103ab79
AlexFilonenko88 <af88_88@mail.ru>
2026-08-25 09:21:37 +0300

    Добавил логирование

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 import aiofiles
 import uuid 
+import logging
 
+
+logger = logging.getLogger(__name__)
 
 ALLOWED_EXTENSIONS = [".png", ".jpg", ".jpeg", ".webp", ".gif"]
 MAX_UPLOAD_SIZE = 5 * 1024 * 1024
@@ -11,6 +14,7 @@ def is_allowed_file(filename: str) -> bool:
     ''' Проверка, являеться файл допуститимым для загрузки '''
 
     ext = Path(filename).suffix.lower()
+    logger.info(f'Получаем расширение файла: {ext}')
 
     return ext in ALLOWED_EXTENSIONS
 
@@ -24,13 +28,14 @@ def get_list_uploaded_images(UPLOAD_DIR):
             if file.is_file()
             and file.suffix.lower() in ALLOWED_EXTENSIONS
         ]
-
+    logger.info(f'Получаем список изображений: {images}')
     return images
 
 
 async def get_unique_name(filename: str) -> str:
     ext = Path(filename).suffix.lower()
     unique_name = f'{uuid.uuid4().hex}{ext}'
+    logger.info(f'Получаем униальное имя изображения: {unique_name}')
 
     return unique_name
     
@@ -43,6 +48,7 @@ async def save_uploaded_file(file, filename, UPLOAD_DIR):
     async with aiofiles.open(file_path, 'wb') as bf:
         while chunk := await file.read(1024*1024):
             await bf.write(chunk)
+            logger.info(f'Сохряняем изображение: {chunk}')
 
 
 if __name__ == '__main__':
